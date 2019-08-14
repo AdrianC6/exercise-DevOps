dedb58c1aafd73b6200329786b39ae65ec2158b0
root <root@localhost.localdomain>
2019-08-14 13:50:56 -0600

    adding all the files

--- exercise-log/src/main/java/cox/ryan/exerciselog/model/repositories/SetJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package cox.ryan.exerciselog.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cox.ryan.exerciselog.model.Set;
-
-public interface SetJpaRepository extends JpaRepository<Set, Integer> {
-
-}
